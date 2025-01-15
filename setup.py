7d43beeac34e8ac2a35f0cf5598e6ab3fae33aa9
Arulkumaran <95573091+arul637@users.noreply.github.com>
2025-01-15 16:51:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='HaulEncryptor',
+    name='haulencryptor',
     version='0.0.1',
     description='A Python-based file encryption and decryption toolkit.',
     long_description=open('README.md').read(),
