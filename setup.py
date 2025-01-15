22ebbe923f1d0c06b26883bdfcf42655d01ddf52
Arulkumaran <95573091+arul637@users.noreply.github.com>
2025-01-15 17:00:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='haul',
+    name='encryptor',
     version='0.1',
     description='A Python-based file encryption and decryption toolkit.',
     long_description=open('README.md').read(),
